8726cf0791ff795fb7bd4f3b939281a7f2d9742b
Dm5trix <50482702+Dm5trix@users.noreply.github.com>
2019-05-10 12:26:33 +0300

    Update make_changer.py

--- make_changer.py
+++ make_changer.py
@@ -1,1 +1,11 @@
+import subprocess
+import optparse
 
+dm_init=optparse.ParseOption()
+
+subprocess.call("ifconfig wlan0",shell=True)
+subprocess.call("ifconfig wlan0 hw ether 00:22:33:44:55:55",shell=True)
+subprocess.call("ifconfig wlan0 up",shell=True)
+
+dm_init.add_option("--i","--interface" ,help="Put your mac adress")
+dm_init.parse_args()
